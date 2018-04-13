7e8822a6ed8edc0f835f4b307743ad7fcf5ec59c
Amanda Tjan <soytjan@gmail.com>
2018-04-13 11:08:47 -0600

    Delete Settings title

--- components/Settings/Settings.js
+++ components/Settings/Settings.js
@@ -22,20 +22,17 @@ export default class Home extends Component <Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.title}>Settings</Text>
-        <View style={styles.statsCont}>
-          <View style={styles.statsDisplay}>
-            <Text style={styles.listItems}>Username: {this.props.screenProps.userEmail}</Text>
-            <Text style={styles.listItems}>Current Points: </Text>
-            <Text style={styles.listItems}>Current Level:</Text>
-          </View>
-          <TouchableOpacity
-            onPress={this.logout}
-            style={styles.logoutBtn}
-          >
-            <Text style={styles.buttonText}>LOGOUT</Text>
-          </TouchableOpacity>
+        <View style={styles.statsDisplay}>
+          <Text style={styles.listItems}>Username: {this.props.screenProps.userEmail}</Text>
+          <Text style={styles.listItems}>Current Points: </Text>
+          <Text style={styles.listItems}>Current Level:</Text>
         </View>
+        <TouchableOpacity
+          onPress={this.logout}
+          style={styles.logoutBtn}
+        >
+          <Text style={styles.buttonText}>LOGOUT</Text>
+        </TouchableOpacity>
       </View>
     )
   }
@@ -50,7 +47,6 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 30,
-    fontFamily: 'Noteworthy',
     textAlign: 'center',
     color: '#3AAFb9',
     marginBottom: 30,
