e4797b6e1fc041210fed5829506913314fd7c183
Ophus Wong <ophus.wong@gmail.com>
2018-04-14 13:27:20 -0600

    Finish tests for Register

--- __tests__/Register-test.js
+++ __tests__/Register-test.js
@@ -11,50 +11,77 @@ describe('REGISTER', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('should call handleRegistration when validateRegistration is called', () => {
-    const mockFn = jest.fn();
-    const wrapper = renderer.create(<Register handleRegistration={mockFn} />);
-    const instance = wrapper.getInstance();
+  describe('REGISTER METHODS', () => {
+    it('should change the alertMsg in state if email and userName are empty strings', () => {
+      const wrapper = renderer.create(<Register />);
+      const instance = wrapper.getInstance();
+      const alertMsg = `Please provide a email and username.`;
 
-    instance.validateRegistration();
+      instance.validateRegistration();
 
+      expect(instance.state.showAlert).toEqual(true);
+      expect(instance.state.alertMsg).toEqual(alertMsg);
+    });
+
+    it('should change the alertMsg in state if password and confirmPassword are empty strings', () => {
+      const wrapper = renderer.create(<Register />);
+      const instance = wrapper.getInstance();
+      alertMsg = `Please add a password.`;
+
+      instance.setState({ email: 'amanda@pophanda.com', userName: 'pandas_rule' });
+      instance.validateRegistration();
+
+      expect(instance.state.alertMsg).toEqual(alertMsg);
+    });
+
+    it('should change the alertMsg in state if password and confirmPassword do not match', () => {
+      const wrapper = renderer.create(<Register />);
+      const instance = wrapper.getInstance();
+      alertMsg = `Please make sure your passwords match.`;
+
+      instance.setState({
+        email: 'amanda@pophanda.com',
+        userName: 'pandas_rule',
+        password: 'panda1',
+        confirmPassword: 'panda'
+      });
+      instance.validateRegistration();
+
+      expect(instance.state.alertMsg).toEqual(alertMsg);
+    });
+
+    it('should call handleRegistration when validateRegistration is called', () => {
+      const mockFn = jest.fn();
+      const mockUser = {
+        email: 'amanda@pophanda.com',
+        userName: 'pandas_rule',
+        password: 'panda1',
+      };
+      const wrapper = renderer.create(<Register handleRegistration={mockFn} />);
+      const instance = wrapper.getInstance();
+
+      instance.setState({
+        email: 'amanda@pophanda.com',
+        userName: 'pandas_rule',
+        password: 'panda1',
+        confirmPassword: 'panda1'
+      });
+      instance.validateRegistration();
+
+      expect(mockFn).toHaveBeenCalledWith(mockUser);
+    });
+
+    it('should change showAlert in state to false if hideAlert is called', () => {
+      const wrapper = renderer.create(<Register />);
+      const instance = wrapper.getInstance();
+
+      expect(instance.state.showAlert).toBe(false);
+
+      instance.setState({ hideAlert: true });
+      instance.hideAlert();
+
+      expect(instance.state.showAlert).toBe(false);
+    });
   });
 
-  it('should change the alertMsg in state if email and userName are empty strings', () => {
-    const wrapper = renderer.create(<Register />);
-    const instance = wrapper.getInstance();
-    const alertMsg = `Please provide a email and username.`;
-
-    instance.validateRegistration();
-
-    expect(instance.state.alertMsg).toEqual(alertMsg);
-  });
-
-  it('should change the alertMsg in state if password and confirmPassword are empty strings', () => {
-    const wrapper = renderer.create(<Register />);
-    const instance = wrapper.getInstance();
-    alertMsg = `Please add a password.`;
-
-    instance.validateRegistration();
-
-    expect(instance.state.alertMsg).toEqual(alertMsg);
-  });
-
-  it('should change the alertMsg in state if password and confirmPassword do not match', () => {
-    const wrapper = renderer.create(<Register />);
-    const instance = wrapper.getInstance();
-
-  });
-
-  it('should change showAlert in state to false if hideAlert is called', () => {
-    const wrapper = renderer.create(<Register />);
-    const instance = wrapper.getInstance();
-
-    expect(instance.state.showAlert).toBe(false);
-
-    instance.setState({ hideAlert: true });
-    instance.hideAlert();
-
-    expect(instance.state.showAlert).toBe(false);
-  });
 });
