61c1ab287afd861148adc5947e72dd8833baa81c
parker lindley <fugazi8@gmail.com>
2018-04-15 13:22:16 -0600

    Create mocks folder and mock firebase

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -3,32 +3,35 @@ import { shallow, configure } from 'enzyme';
 import App from '../components/App/App';
 import Adapter from 'enzyme-adapter-react-16';
 import renderer from 'react-test-renderer';
+import firebase from 'react-native-firebase'
 
 configure({ adapter: new Adapter() });
 
-jest.mock('../components/App/App', () => {
-  const firebasemock = require('firebase-mock');
-
-  const mockdatabase = new firebasemock.MockFirebase();
-  const mockauth = new firebasemock.MockFirebase();
-  const mocksdk = new firebasemock.MockFirebaseSdk(path => {
-    return path ? mockdatabase.child(path) : mockdatabase;
-  }, () => {
-    return mockauth;
-  });
-  const firebaseApp = mocksdk.initializeApp(); // can take a path arg to database url
-  return {
-    isMock: true,
-    db: firebaseApp.database()
-  }
-});
-
+// jest.mock('../components/App/App', () => {
+//   const firebasemock = require('firebase-mock');
+//   const mockauth = new firebasemock.MockFirebase();
+//   const mocksdk = new firebasemock.MockFirebaseSdk(path => {
+//     return path ? mockdatabase.child(path) : mockdatabase;
+//   }, () => {
+//     return mockauth;
+//   });
+//   const firebaseApp = mocksdk.initializeApp(); // can take a path arg to database url
+//   return {
+//     isMock: true,
+//     db: firebaseApp.database()
+//   }
+// });
 describe('App', () => {
 
   it('renders correctly', () => {
-    const wrap = <App />
-    const wrapper = renderer.create(wrap.$$typeof);    
-    expect(wrap).toMatchSnapshot();
+    const wrapper = renderer.create(<App />).toJSON();
+    expect(wrapper).toMatchSnapshot();
+  })
+
+  it('should have methods', () => {
+    const wrapper = renderer.create(<App />);
+    const inst = wrapper.getInstance()
+    console.log(inst);
   })
 
 })
