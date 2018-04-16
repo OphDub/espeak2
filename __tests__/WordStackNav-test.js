360ad7e152674f2f48a2ccbff0ec7ac4cee87db1
Ophus Wong <ophus.wong@gmail.com>
2018-04-16 16:20:59 -0600

    Update WordStackNav test and snapshots

--- __tests__/WordStackNav-test.js
+++ __tests__/WordStackNav-test.js
@@ -3,14 +3,25 @@ import { shallow, configure } from 'enzyme';
 import WordStackNav from '../components/WordStackNav/WordStackNav';
 import Adapter from 'enzyme-adapter-react-16';
 import renderer from 'react-test-renderer';
+import decks from '../mockData/decks';
 configure({ adapter: new Adapter() });
 
-describe('Card', () => {
-
+describe('WORD STACK NAV', () => {
   it('renders correctly', () => {
-    const mockProps = {handlePoints: jest.fn(), userPoints: 0, updateUserPoints: jest.fn()}
+    const user = {
+      username: 'opa',
+      email: 'opa@startup.com',
+      points: 0,
+      stack_id: 1,
+      firebase_id: 12345
+    };
+    const mockProps = {
+      handlePoints: jest.fn(),
+      user,
+      updateUserPoints: jest.fn(),
+      decks,
+    }
     const wrap = renderer.create(<WordStackNav screenProps={mockProps} />).toJSON();
     expect(wrap).toMatchSnapshot();
-  })
-
-})
+  });
+});
