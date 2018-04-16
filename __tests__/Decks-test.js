999b3d9429db61155d1cd40459a5351ab7d71773
Ophus Wong <ophus.wong@gmail.com>
2018-04-16 13:00:51 -0600

    Update render test for Decks

--- __tests__/Decks-test.js
+++ __tests__/Decks-test.js
@@ -3,10 +3,24 @@ import { shallow, configure } from 'enzyme';
 import Decks from '../components/Decks/Decks';
 import Adapter from 'enzyme-adapter-react-16';
 import renderer from 'react-test-renderer';
+import decks from '../mockData/decks';
 configure({ adapter: new Adapter() });
 
-it('renders correctly', () => {
-  const word = { english: 'hi', spanish: 'hola', hint: 'o-la' };
-  const wrap = renderer.create(<Decks />).toJSON();
-  expect(wrap).toMatchSnapshot();
-})
+describe('DECKS', () => {
+  it('renders correctly', () => {
+    const user = {
+      username: 'pophus',
+      email: 'pophus@notpophanda.com',
+      points: 0,
+      stack_id: 1,
+      firebase_id: 12345
+    };
+    const mockScreenProps = {
+      decks,
+      user,
+    };
+
+    const wrap = renderer.create(<Decks screenProps={mockScreenProps}/>).toJSON();
+    expect(wrap).toMatchSnapshot();
+  });
+});
