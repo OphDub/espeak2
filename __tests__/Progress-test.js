bc5768d0b92aab7cb8543987203fb84221868976
Ophus Wong <ophus.wong@gmail.com>
2018-04-17 11:02:22 -0600

    Lint Progress, add describe block in Progress test, update Progress snapshot

--- __tests__/Progress-test.js
+++ __tests__/Progress-test.js
@@ -5,19 +5,21 @@ import Adapter from 'enzyme-adapter-react-16';
 import renderer from 'react-test-renderer';
 configure({ adapter: new Adapter() });
 
-it('renders correctly', () => {
-  const mockProp = {category: 'basics 1'}
-  const wrap = renderer.create(<Progress stack={mockProp}/>).toJSON();
-  expect(wrap).toMatchSnapshot();
-})
+describe('PROGRESS', () => {
+  it('renders correctly', () => {
+    const mockProp = {category: 'basics 1'}
+    const wrap = renderer.create(<Progress stack={mockProp}/>).toJSON();
+    expect(wrap).toMatchSnapshot();
+  });
 
-it('it renders when a new stack is passed in as props', () => {
-  const mockProp = { category: 'basics 1' }
-  const mockPropTwo = { category: 'verbs' }
-  const wrapper = shallow(
-    <Progress stack={mockProp} />
-  );
-  expect(wrapper).toMatchSnapshot();
-  wrapper.setProps({ stack: mockPropTwo });
-  expect(wrapper).toMatchSnapshot();
+  it('it renders when a new stack is passed in as props', () => {
+    const mockProp = { category: 'basics 1' }
+    const mockPropTwo = { category: 'verbs' }
+    const wrapper = shallow(
+      <Progress stack={mockProp} />
+    );
+    expect(wrapper).toMatchSnapshot();
+    wrapper.setProps({ stack: mockPropTwo });
+    expect(wrapper).toMatchSnapshot();
+  });
 });
