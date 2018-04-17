a526a89e5942e49de39ba46a52002631bcf36bc4
Amanda Tjan <soytjan@gmail.com>
2018-04-17 11:10:38 -0600

    Write tests for handleHintPoints in App

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -28,7 +28,9 @@ describe('App', () => {
       const inst = wrapper.getInstance();
 
       expect(inst.state.showAlert).toEqual(false);
+
       inst.showAlert();
+
       expect(inst.state.showAlert).toEqual(true);
     })
 
@@ -37,8 +39,11 @@ describe('App', () => {
       const inst = wrapper.getInstance();
 
       inst.setState({showAlert: true});
+
       expect(inst.state.showAlert).toEqual(true);
+
       inst.hideAlert();
+
       expect(inst.state.showAlert).toEqual(false);
     })
 
@@ -54,9 +59,32 @@ describe('App', () => {
       };
 
       inst.setState({ user });
+
       expect(inst.state.user.points).toEqual(0);
+
       inst.handlePoints();
+
       expect(inst.state.user.points).toEqual(10);
+    })
+
+    it('the handleHintPoints function should deduct 5 points from the user', () => {
+      const wrapper = renderer.create(<App />);
+      const instance = wrapper.getInstance();
+      const user = {
+        username: 'ophanda', 
+        email: 'ophanda@betterthanpophus.com', 
+        points: 10, 
+        stack_id: 1, 
+        firebase_id: 12345
+      };
+
+      instance.setState({ user });
+
+      expect(instance.state.user.points).toEqual(10);
+
+      instance.handleHintPoints();
+
+      expect(instance.state.user.points).toEqual(5);
     })
   
     it('the handleLogin function should set the loading state to true and call beLogin and update the state', async () => {
@@ -75,6 +103,7 @@ describe('App', () => {
       const inst = wrapper.getInstance();
 
       await inst.handleLogin('pophus', 'password');
+
       expect(inst.state.loading).toEqual(false);
       expect(inst.state.user).toEqual({ "email": "pophus@notpophanda.com", "firebase_id": 12345, "points": 0, "stack_id": 1, "username": "pophus" })
     })
@@ -93,8 +122,10 @@ describe('App', () => {
       }));
       const wrapper = renderer.create(<App />);
       const inst = wrapper.getInstance();
+
       expect(inst.state.user).toEqual(null);
       await inst.beLogin(2);
+
       expect(inst.state.user).toEqual(user)
     })
 
@@ -108,9 +139,13 @@ describe('App', () => {
         stack_id: 1,
         firebase_id: 12345
       };
+
       inst.setState({user});
+
       expect(inst.state.user).toBeDefined();
+
       inst.handleSignOut();
+
       expect(inst.state.user).toEqual(null);
     })
 
@@ -121,7 +156,9 @@ describe('App', () => {
       }))
       const wrapper = renderer.create(<App />);
       const inst = wrapper.getInstance();
+
       await inst.handleRegistration({email: 'pophus@notpophanda.com', userName: 'pophus long', password: 'password', uid: 54321});
+      
       expect(inst.state.user).toEqual({"email": "pophus@notpophanda.com", "firebase_id": 54321, "name": "pophus long", "points": 0, "stack_id": 1 })
     })
   })
